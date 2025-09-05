3b4bc5671d2e4c0334b07d550f592a66231ea68b
nowwie <novitazkaa@gmail.com>
2025-09-06 00:16:12 +0700

    feat: gradle fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.1.4" apply false
+    id("com.android.application") version "8.7.0" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false 
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
